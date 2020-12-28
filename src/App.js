13ad93e0c28a4db9dfa423d25d449f7976d53f0c
nabesuke <nabesuke5098@gmail.com>
2020-12-28 16:43:53 +0900

    [modified]multiple-states

--- src/App.js
+++ src/App.js
@@ -1,30 +1,27 @@
 import React, { Fragment, useState } from 'react';
 
-const App = () =>  {
-  const [count, setCount] = useState(0)
+const App = props =>  {
+  const [name, setName] = useState(props.name)
+  const [price, setPrice] = useState(props.price)
 
-  const increment = () => setCount(count + 1)
-  const decrement = () => setCount(count - 1)
-  const increment2 = () => setCount(previousCount => previousCount + 1)
-  const decrement2 = () => setCount(previousCount => previousCount - 1)
-  const resetCounter = () => setCount(0)
-  const timesCounter = () => setCount(count * 2)
-  const threeCount = () => setCount(previousCount => previousCount % 3 === 0 ? previousCount / 3 : previousCount)
-
+  const reset = () => {
+    setPrice(props.price)
+    setName(props.name)
+  }
   return (
-    <Fragment>
-      <div>
-        count: {count}
-      </div>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={increment2}>+1</button>
-      <button onClick={decrement2}>-1</button>
-      <button onClick={resetCounter}>reset</button>
-      <button onClick={timesCounter}>×2</button>
-      <button onClick={threeCount}>3</button>
-    </Fragment>
-  );
+    <>
+      <p>現在の{name}は{price}です。</p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={e => setName(e.target.value)} />
+    </>
+  )
+}
+
+App.defaultProps = {
+  name: '',
+  price: 1000
 }
 
 export default App;
