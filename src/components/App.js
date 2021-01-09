48b9dacfc3bc68bc49f826e8d047a678b1de645b
nabesuke <nabesuke5098@gmail.com>
2021-01-09 18:38:29 +0900

    [useContext]

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,21 @@
 import React, { useReducer } from 'react';
 import reducer from '../reducers';
+import AppContext from '../contexts/AppContext';
 import Events from './Events';
 import EventForm from './EventForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+console.log({ AppContext });
 const App = () =>  {
   const [state, dispatch] = useReducer(reducer,[]);
   console.log(state, ' in App.js');
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch} />
-      <Events state={state} dispatch={dispatch} />
-    </div>
+    <AppContext.Provider value={'Hello, Provider'}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch} />
+        <Events state={state} dispatch={dispatch} />
+      </div>
+    </AppContext.Provider>
   );
 }
 
