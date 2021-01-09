cd0afa567d2db360992d3f0b0359f665a7791f13
nabesuke <nabesuke5098@gmail.com>
2021-01-09 18:58:52 +0900

    [refactor prop drilling]

--- src/components/App.js
+++ src/components/App.js
@@ -4,16 +4,15 @@ import AppContext from '../contexts/AppContext';
 import Events from './Events';
 import EventForm from './EventForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
-console.log({ AppContext });
+
 const App = () =>  {
   const [state, dispatch] = useReducer(reducer,[]);
-  console.log(state, ' in App.js');
 
   return (
-    <AppContext.Provider value={'Hello, Provider'}>
+    <AppContext.Provider value={{ state, dispatch }}>
       <div className="container-fluid">
-        <EventForm state={state} dispatch={dispatch} />
-        <Events state={state} dispatch={dispatch} />
+        <EventForm />
+        <Events />
       </div>
     </AppContext.Provider>
   );
