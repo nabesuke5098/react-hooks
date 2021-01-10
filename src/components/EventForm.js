dac4fa033442217e578cc64a131df23c99713d7d
nabesuke <nabesuke5098@gmail.com>
2021-01-10 09:37:58 +0900

    [create operationLogs]

--- src/components/EventForm.js
+++ src/components/EventForm.js
@@ -1,6 +1,12 @@
 import React, { useState, useContext } from 'react';
-import { CREATE_EVENT, DELETE_ALL_EVENTS } from '../actions';
+import { 
+  CREATE_EVENT, 
+  DELETE_ALL_EVENTS,
+  ADD_OPERATION_LOG,
+  DELETE_ALL_OPERATION_LOGS
+} from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const EventForm = () => {
   const { state, dispatch } = useContext(AppContext);
@@ -15,6 +21,13 @@ const EventForm = () => {
       title,
       body
     });
+
+    dispatch({
+      type: ADD_OPERATION_LOG,
+      description: 'イベントを作成しました。',
+      operatedAt: timeCurrentIso8601()
+    });
+
     setTitle('');
     setBody('');
   }
@@ -22,7 +35,14 @@ const EventForm = () => {
   const deleteAllEvents = e => {
     e.preventDefault();
     const result = window.confirm('全てのイベントを削除しますか？');
-    if (result) dispatch({ type: DELETE_ALL_EVENTS });
+    if (result) {
+      dispatch({ type: DELETE_ALL_EVENTS });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: '全てのイベントを削除しました。',
+        operatedAt: timeCurrentIso8601()
+      });
+    }
   }
 
   const unCreatable = title === '' || body === '' ;
