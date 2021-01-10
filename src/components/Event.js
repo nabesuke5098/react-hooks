b57ae2f8041153466b6341a4d7739f3186a00ccc
nabesuke <nabesuke5098@gmail.com>
2021-01-10 09:58:56 +0900

    [create-operation-deletion]

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,13 +1,24 @@
 import React, { useContext } from 'react';
-import { DELETE_EVENT } from '../actions';
+import { 
+  DELETE_EVENT,
+  ADD_OPERATION_LOG
+} from '../actions';
 import AppContext from '../contexts/AppContext';
+import { timeCurrentIso8601 } from '../utils';
 
 const Event = ({ event }) => {
   const { dispatch } = useContext(AppContext);
   const id = event.id;
   const handleClickDeleteButton = () => {
     const result = window.confirm(`id:${id}のイベントを削除しますか？`)
-    if (result) dispatch({ type: DELETE_EVENT, id });
+    if (result) {
+      dispatch({ type: DELETE_EVENT, id });
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: `id:${id}のイベントを削除しました。`,
+        operatedAt: timeCurrentIso8601()
+      });
+    }
   }
   return (
     <tr>
